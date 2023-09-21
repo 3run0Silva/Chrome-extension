f95bcdbdf834f0c0fc7bd972b0f019f8bb38a9c9
Bruno Da Silva <138171223+User3ds@users.noreply.github.com>
2023-09-21 13:52:45 +0200

    JS comments
    1- Added some comments to the JS file for code comprehension and readability.
    
    This is the last commit of the project from this point on I'm not expecting any more changes.

--- script.js
+++ script.js
@@ -1,49 +1,55 @@
-"use strict"
+"use strict";
 
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-const tabBtn = document.getElementById("tab-btn")
+let myLeads = []; // Store website leads
+const inputEl = document.getElementById("input-el"); 
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
 
+// Populate leads from local storage, if available
 if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
 }
 
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-})
+// Event listener for adding the current tab as a lead
+tabBtn.addEventListener("click", function() {    
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+    });
+});
 
+// Render the list of leads
 function render(leads) {
-    let listItems = ""
+    let listItems = "";
     for (let i = 0; i < leads.length; i++) {
+        // Create list items with links to the leads
         listItems += `
             <li>
                 <a target='_blank' href='${leads[i]}'>
                     ${leads[i]}
                 </a>
             </li>
-        `
+        `;
     }
-    ulEl.innerHTML = listItems
+    ulEl.innerHTML = listItems;
 }
 
+// Event listener for clearing leads on double-click
 deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
-})
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+});
 
+// Event listener for adding a lead from the input field
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
-})
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+});
